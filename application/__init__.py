00b93de5fd6705d610f697e8cec2b0372f480ed8
Dennis <dennsva@gmail.com>
2018-03-26 12:20:02 +0300

    herokua testaillaan

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,14 @@ app = Flask(__name__)
 
 # database
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -34,4 +40,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # create database tables
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
